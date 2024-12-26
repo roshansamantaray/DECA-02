d1762e49b941d14d85325c769878e463427d6c37
Roshan Samantaray <roshansamantaray.5813@outlook.com>
2024-12-26 15:59:41 +0100

    Two test cases successfully passing for Exercise 1. polymorphicCallSite and instanceToInterfaceMethod throwing failures.

--- src/main/java/analysis/exercise1/CHAAlgorithm.java
+++ src/main/java/analysis/exercise1/CHAAlgorithm.java
@@ -4,6 +4,15 @@ import analysis.CallGraph;
 import analysis.CallGraphAlgorithm;
 import java.util.*;
 import javax.annotation.Nonnull;
+
+import sootup.core.jimple.common.expr.AbstractInvokeExpr;
+import sootup.core.jimple.common.expr.JSpecialInvokeExpr;
+import sootup.core.jimple.common.expr.JVirtualInvokeExpr;
+import sootup.core.jimple.common.stmt.JInvokeStmt;
+import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.model.SootClassMember;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.ClassType;
 import sootup.java.core.views.JavaView;
 
 public class CHAAlgorithm extends CallGraphAlgorithm {
@@ -20,6 +29,43 @@ public class CHAAlgorithm extends CallGraphAlgorithm {
     // To get your entry points we prepared getEntryPoints(view) in the superclass for you
 
     // TODO: implement
+    getEntryPoints(view).forEach(entryPoint -> {
+      cg.addNode(entryPoint);
 
+      view.getMethod(entryPoint).ifPresent(method -> {
+        if (method.hasBody()) {
+          for (Stmt stmt : method.getBody().getStmtGraph().getStmts()) {
+            if (stmt instanceof JInvokeStmt) {
+              JInvokeStmt invokeStmt = (JInvokeStmt) stmt;
+              AbstractInvokeExpr invokeExpr = invokeStmt.getInvokeExpr();
+
+              if (invokeExpr instanceof JSpecialInvokeExpr) {
+                MethodSignature target = invokeExpr.getMethodSignature();
+                addNodeAndEdge(cg, entryPoint, target);
+              }
+
+              if (invokeExpr instanceof JVirtualInvokeExpr) {
+                JVirtualInvokeExpr virtualInvoke = (JVirtualInvokeExpr) invokeExpr;
+                String methodName = virtualInvoke.getMethodSignature().getName();
+
+                view.getClass((ClassType) virtualInvoke.getBase().getType()).ifPresent(clazz -> {
+                  clazz.getMethods().stream()
+                          .filter(m -> m.getName().equals(methodName))
+                          .map(SootClassMember::getSignature)
+                          .forEach(target -> addNodeAndEdge(cg, entryPoint, target));
+                });
+              }
+            }
+          }
+        }
+      });
+    });
+  }
+
+  private void addNodeAndEdge(@Nonnull CallGraph cg, @Nonnull MethodSignature source, @Nonnull MethodSignature target) {
+    if (!cg.hasNode(target)) {
+      cg.addNode(target);
+    }
+    cg.addEdge(source, target);
   }
 }
